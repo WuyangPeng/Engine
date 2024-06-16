378404a07b19ede2e1f67ee9dbdfb3e0d7308412
彭曄恩 <94458936@qq.com>
2024-06-16 17:34:08 +0800

    * 配置文件自动复制

--- TheLastOverlord/x64/DebugWindows/Resource/Shader/AmbientLightEffect.ps.glsl
+++ TheLastOverlord/x64/DebugWindows/Resource/Shader/AmbientLightEffect.ps.glsl
@@ -1,7 +1,7 @@
-  layout(location = 0) in vec4 vertexColor;
-  layout(location = 0) out vec4 pixelColor;
+layout(location = 0) in vec4 vertexColor;
+layout(location = 0) out vec4 pixelColor;
   
-  void main()
-  {
+void main()
+{
     pixelColor = vertexColor;
-  };
+};
