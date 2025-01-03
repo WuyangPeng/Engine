b1f9a78626ba6b62d52c776aa2ff11bdccd5ca96
彭武阳 <94458936@qq.com>
2025-01-03 19:58:35 +0800

    * 工程文件整合

---
+++ Bat/CopyProjectConfiguration.bat
@@ -0,0 +1,13 @@
+@echo on
+ 
+set ProjectDir=%1
+set ProjectName=%2
+set Target=%3
+
+xcopy "%ProjectDir%/Configuration/EnvironmentVariable.json" "%Target%" /F /Y
+xcopy "%ProjectDir%/Configuration/%ProjectName%/Renderer.json" "%Target%/%ProjectName%/" /F /Y
+xcopy "%ProjectDir%/Configuration/Project.json" "%Target%" /F /Y
+xcopy "%ProjectDir%/Configuration/%ProjectName%.json" %Target%" /F /Y
+xcopy "%ProjectDir%/Configuration/Global.json" "%Target%" /F /Y
+xcopy "%ProjectDir%/Configuration/Smtp.json" "%Target%" /F /Y
+xcopy "%ProjectDir%/Configuration/Database.json" "%Target%" /F /Y
