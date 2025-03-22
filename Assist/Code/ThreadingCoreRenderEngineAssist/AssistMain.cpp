e068d0d82266a208cc213b128e655814dfe11b41
彭武阳 <94458936@qq.com>
2025-03-22 12:56:25 +0800

    * 辅助工程整理

--- Assist/Code/ThreadingCoreRenderEngineAssist/AssistMain.cpp
+++ Assist/Code/ThreadingCoreRenderEngineAssist/AssistMain.cpp
@@ -1,11 +1,11 @@
-/// Copyright (c) 2010-2024
+/// Copyright (c) 2010-2025
 /// Threading Core Render Engine
 ///
 /// 作者：彭武阳，彭晔恩，彭晔泽
 /// 联系作者：94458936@qq.com
 ///
 /// 标准：std:c++20
-/// 版本：1.0.1.2 (2024/10/16 20:45)
+/// 版本：1.0.2.0 (2025/03/22 11:10)
 
 #include "AssistHelper.h"
 #include "Framework/Helper/MainFunctionMacro.h"
