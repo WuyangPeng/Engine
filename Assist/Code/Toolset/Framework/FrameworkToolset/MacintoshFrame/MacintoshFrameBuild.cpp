5716d7063437d48498b49d9e247c0a6d3df1a733
彭武阳 <94458936@qq.com>
2025-04-03 20:49:14 +0800

    + 通过linux编译

--- Assist/Code/Toolset/Framework/FrameworkToolset/MacintoshFrame/MacintoshFrameBuild.cpp
+++ Assist/Code/Toolset/Framework/FrameworkToolset/MacintoshFrame/MacintoshFrameBuild.cpp
@@ -16,7 +16,7 @@
 
 CLASS_INVARIANT_STUB_DEFINE(FrameworkToolset, MacintoshFrameBuild)
 
-namespace FrameworkToolset
+namespace Framework
 {
     template class Framework::MacintoshFrameBuild<Framework::MacintoshProcessInterface>;
 }
