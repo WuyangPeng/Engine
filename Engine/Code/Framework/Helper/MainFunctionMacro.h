fe3a1f817c0acc3446997371ebfb899c8105b554
彭武阳 <94458936@qq.com>
2025-04-10 14:21:04 +0800

    + 宏修改

--- Engine/Code/Framework/Helper/MainFunctionMacro.h
+++ Engine/Code/Framework/Helper/MainFunctionMacro.h
@@ -34,13 +34,13 @@
     #define WINDOWS_MAIN_FUNCTION_WITH_INFORMATION(namespaceName, helperClassName, windowName, engineEnvironment, engineDirectory, windowSizeWidth, windowSizeHeight) \
         int main(int argc, char** argv)                                                                                                                               \
         {                                                                                                                                                             \
-            return Framework::MainEntryPoint<namespaceName::helperClassName>(argc, argv, "", engineEnvironment, engineDirectory);                                     \
+            return Framework::MainEntryPoint<namespaceName::helperClassName>(argc, argv, "", "", "");                                                                 \
         }
 
-    #define WINDOWS_MAIN_FUNCTION_USE_PARAMETER(namespaceName, helperClassName, engineEnvironment, engineDirectory, renderer)     \
-        int main(int argc, char** argv)                                                                                           \
-        {                                                                                                                         \
-            return Framework::MainEntryPoint<namespaceName::helperClassName>(argc, argv, "", engineEnvironment, engineDirectory); \
+    #define WINDOWS_MAIN_FUNCTION_USE_PARAMETER(namespaceName, helperClassName, engineEnvironment, engineDirectory, renderer) \
+        int main(int argc, char** argv)                                                                                       \
+        {                                                                                                                     \
+            return Framework::MainEntryPoint<namespaceName::helperClassName>(argc, argv, "", "", "");                         \
         }
 
 #endif  // SYSTEM_PLATFORM_WIN32
