0fb4d03f93e4115398faf752f2188d5bc3e583f6
彭武阳 <94458936@qq.com>
2025-04-16 19:42:53 +0800

    + 测试代码

--- Engine/Code/CoreTools/Helper/PropertiesMacro.h
+++ Engine/Code/CoreTools/Helper/PropertiesMacro.h
@@ -83,7 +83,7 @@
 
     #define TCRE_PROPERTY_READ(type, name, function) CoreTools::PropertyGetExternal<ClassType, const type&, &ClassType::function> name
     #define TCRE_PROPERTY_WRITE(type, name, function) CoreTools::PropertySetExternal<ClassType, const type&, &ClassType::function> name
-    #define TCRE_PROPERTY_READ_WRITE(type, name, getFunction, setFunction) CoreTools::PropertyGetSetExternal<ClassType, const type&, &ClassType::getFunction, type&, &ClassType::setFunction> name
+    #define TCRE_PROPERTY_READ_WRITE(type, name, getFunction, setFunction) CoreTools::PropertyGetSetExternal<ClassType, const type&, &ClassType::getFunction, const type&, &ClassType::setFunction> name
 
 #endif  // TCRE_USE_BORLANDC
 
