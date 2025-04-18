05a2fb30c6ef3c8ea9e6c1decbc53c49cb443efd
彭武阳 <94458936@qq.com>
2025-04-18 21:04:35 +0800

    + 测试代码

--- EngineTesting/Code/Rendering/RenderingWindowTesting/TestingMain.cpp
+++ EngineTesting/Code/Rendering/RenderingWindowTesting/TestingMain.cpp
@@ -10,7 +10,7 @@
 #include "Rendering/RenderingWindowTesting/TestingHelper.h"
 #include "Framework/Helper/MainFunctionMacro.h"
 
-#include <vld.h>
+#include "System/Helper/PragmaWarning/Vld.h"
 
 WINDOWS_MAIN_FUNCTION_WITH_INFORMATION(RenderingWindowTesting,
                                        TestingHelper,
