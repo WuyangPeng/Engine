6ce17354683f9187d4ffe9a36497f038851192ce
彭武阳 <94458936@qq.com>
2025-04-18 20:15:59 +0800

    + 测试代码

---
+++ Bat/compile.sh
@@ -0,0 +1,14 @@
+cd Libs/Bat
+sh create_lib.sh
+
+cd ../../Engine/Bat
+sh complie.sh
+
+cd ../../Assist/Bat
+sh complie.sh
+
+cd ../../EngineTesting/Bat
+sh complie.sh
+
+cd ../../AssistTesting/Bat
+sh complie.sh
