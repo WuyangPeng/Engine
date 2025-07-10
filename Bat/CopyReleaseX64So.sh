ee04001d3a6d53ea0f9ea68188503190618de7c0
彭武阳 <94458936@qq.com>
2025-07-10 15:16:08 +0800

    + cmake文件

--- Bat/CopyReleaseX64So.sh
+++ Bat/CopyReleaseX64So.sh
@@ -1,5 +1,4 @@
 @echo on
  
 cp -f "/data/coding/Libs/ACE/ACE_wrappers/lib/libACE.so" "/data/coding/EngineTesting/x64/CMake/ReleaseWindows/" 
-cp -f "/data/coding/Libs/mysql-connector-c++/build/*.so" "/data/coding/EngineTesting/x64/CMake/ReleaseWindows/" 
-cp -f "/data/coding/Libs/activemq/src/main/.libs/*.so" "/data/coding/EngineTesting/x64/CMake/ReleaseWindows/" 
+cp -f "/data/coding/Libs/activemq/src/main/.libs/libactivemq-cpp.so" "/data/coding/EngineTesting/x64/CMake/ReleaseWindows/" 
