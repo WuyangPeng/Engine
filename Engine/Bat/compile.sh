3f3eb72ddd29ec4e53e729e8fad85ea168fb48c2
彭武阳 <94458936@qq.com>
2025-07-10 15:51:18 +0800

    + cmake文件

--- Engine/Bat/compile.sh
+++ Engine/Bat/compile.sh
@@ -3,9 +3,9 @@ cd ../EngineCMake
 mkdir -p build-debug build-release
 
 cd build-debug
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Debug ../
 make
 
 cd ../build-release
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release ../
 make 
