72607a810612ddb935954095815f19f365ca7c38
彭武阳 <94458936@qq.com>
2025-07-10 15:13:29 +0800

    + cmake文件

--- Bat/CopyReleaseX64So.sh
+++ Bat/CopyReleaseX64So.sh
@@ -1,5 +1,5 @@
 @echo on
  
-cp -f "/data/coding/Libs/ACE/ACE_wrappers/lib/libACE.so" "/data/coding/EngineTesting/x64/CMake/ReleaseWindows/" /F /Y
-cp -f "/data/coding/Libs/mysql-connector-c++/build/*.so" "/data/coding/EngineTesting/x64/CMake/ReleaseWindows/" /F /Y
-cp -f "/data/coding/Libs/activemq/src/main/.libs/*.so" "/data/coding/EngineTesting/x64/CMake/ReleaseWindows/" /F /Y
+cp -f "/data/coding/Libs/ACE/ACE_wrappers/lib/libACE.so" "/data/coding/EngineTesting/x64/CMake/ReleaseWindows/" 
+cp -f "/data/coding/Libs/mysql-connector-c++/build/*.so" "/data/coding/EngineTesting/x64/CMake/ReleaseWindows/" 
+cp -f "/data/coding/Libs/activemq/src/main/.libs/*.so" "/data/coding/EngineTesting/x64/CMake/ReleaseWindows/" 
