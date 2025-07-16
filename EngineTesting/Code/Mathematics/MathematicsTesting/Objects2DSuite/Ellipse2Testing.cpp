99eb430bc6386d15ccdc1ba515f58068ce58ecda
彭武阳 <94458936@qq.com>
2025-07-16 16:58:33 +0800

    + cmake文件

--- EngineTesting/Code/Mathematics/MathematicsTesting/Objects2DSuite/Ellipse2Testing.cpp
+++ EngineTesting/Code/Mathematics/MathematicsTesting/Objects2DSuite/Ellipse2Testing.cpp
@@ -12,6 +12,7 @@
 #include "CoreTools/Helper/ClassInvariant/MathematicsClassInvariantMacro.h"
 #include "CoreTools/UnitTestSuite/UnitTestDetail.h"
 #include "Mathematics/Algebra/Vector2ToolsDetail.h"
+#include "Mathematics/Algebra/Vector2Detail.h"
 #include "Mathematics/Objects2D/Ellipse2Detail.h"
 
 #include <random>
