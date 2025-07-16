f991bc7763b18243a06f0d4b14ec6dd2d0d30492
彭武阳 <94458936@qq.com>
2025-07-16 13:43:42 +0800

    + cmake文件

--- EngineTesting/Code/Network/NetworkTesting/NetworkMessageSuite/JsonMessageTesting.cpp
+++ EngineTesting/Code/Network/NetworkTesting/NetworkMessageSuite/JsonMessageTesting.cpp
@@ -21,6 +21,7 @@
 #include "Network/NetworkMessage/MessageTypeCondition.h"
 #include "Network/NetworkMessage/NullMessage.h"
 #include "Network/NetworkMessage/MessageSourceDetail.h"
+#include "Network/NetworkMessage/MessageContainerDetail.h"
 
 using System::operator|;
 
