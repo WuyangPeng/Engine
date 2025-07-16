a255fff26bebc9b9438e86ad28bf83fc6fb662b0
彭武阳 <94458936@qq.com>
2025-07-16 11:26:59 +0800

    + cmake文件

--- EngineTesting/Code/CoreTools/CoreToolsTesting/ConsoleSuite/ConsoleTextColorsManagerTesting.cpp
+++ EngineTesting/Code/CoreTools/CoreToolsTesting/ConsoleSuite/ConsoleTextColorsManagerTesting.cpp
@@ -13,6 +13,7 @@
 #include "CoreTools/Helper/AssertMacro.h"
 #include "CoreTools/Helper/ClassInvariant/CoreToolsClassInvariantMacro.h"
 #include "CoreTools/UnitTestSuite/UnitTestDetail.h"
+#include "CoreTools/Console/ConsoleInstanceDetail.h"
 
 #include <iostream>
 
