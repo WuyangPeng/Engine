1d37a8c611551494cc4877e1271f6488d50596a8
彭武阳 <94458936@qq.com>
2025-07-16 17:06:45 +0800

    + cmake文件

--- EngineTesting/Code/Mathematics/MathematicsTesting/IntersectionSuite/Intersection1D/StaticTestIntersector1Testing.cpp
+++ EngineTesting/Code/Mathematics/MathematicsTesting/IntersectionSuite/Intersection1D/StaticTestIntersector1Testing.cpp
@@ -12,6 +12,7 @@
 #include "CoreTools/Helper/ClassInvariant/MathematicsClassInvariantMacro.h"
 #include "CoreTools/UnitTestSuite/UnitTestDetail.h"
 #include "Mathematics/Intersection/StaticTestIntersector1Detail.h"
+#include "Mathematics/Algebra/Vector2Detail.h"
 
 #include <random>
 
