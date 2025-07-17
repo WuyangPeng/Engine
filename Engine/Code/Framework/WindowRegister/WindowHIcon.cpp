9575bc017fd6dbe883a9c30e57da03397237f48a
彭武阳 <94458936@qq.com>
2025-07-17 12:45:36 +0800

    + cmake文件

--- Engine/Code/Framework/WindowRegister/WindowHIcon.cpp
+++ Engine/Code/Framework/WindowRegister/WindowHIcon.cpp
@@ -38,11 +38,11 @@ void Framework::WindowHIcon::CheckUpHIcon() noexcept
     {
         hIcon = System::LoadSystemIcon(nullptr, System::gApplication);
 
-        #ifdef TCRE_SYSTEM_PLATFORM
+        #ifdef SYSTEM_PLATFORM_WIN32
 
         LOG_SINGLETON_ENGINE_APPENDER(Warn, Framework, SYSTEM_TEXT("º”‘ÿÕº±Í ß∞‹£°"), CoreTools::LogAppenderIOManageSign::TriggerAssert);
 
-        #endif // TCRE_SYSTEM_PLATFORM
+        #endif // SYSTEM_PLATFORM_WIN32
     }
 }
 
