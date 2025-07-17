7941c0a103717a676629069b703c3e0a31eb9e49
彭武阳 <94458936@qq.com>
2025-07-17 16:25:53 +0800

    + 增加日志

--- Engine/Code/CoreTools/Base/Singleton.h
+++ Engine/Code/CoreTools/Base/Singleton.h
@@ -56,7 +56,7 @@ namespace CoreTools
         void DeleteSingleton() const noexcept(gAssert < 2 || gCoreToolsAssert < 2);
         static void CheckSingleton() noexcept(gAssert < 0 || gCoreToolsAssert < 0);
 
-    private:
+    protected:
         static PointType singleton;
     };
 }
