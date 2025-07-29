4ef7e0c672519c8c3864f9c2000cda90b18b3efe
彭武阳 <94458936@qq.com>
2025-07-29 16:07:49 +0800

    升级openssl库

---
+++ External/Bat/opensslBat/1. CopyOpenssl.bat
@@ -0,0 +1,29 @@
+@echo on
+
+rmdir /S /Q "../../../Libs/openssl"
+
+xcopy "../../openssl/Win32/Debug/bin/*" "../../../Libs/openssl/Win32/Debug/bin/" /F /Y /E 
+xcopy "../../openssl/Win32/Debug/include/openssl/*.h" "../../../Libs/openssl/Win32/Debug/include/openssl/" /F /Y /E 
+xcopy "../../openssl/Win32/Debug/lib/*.lib" "../../../Libs/openssl/Win32/Debug/lib/" /F /Y /E 
+xcopy "../../openssl/Win32/Debug/lib/ossl-modules/*.dll" "../../../Libs/openssl/Win32/Debug/lib/ossl-modules/" /F /Y /E 
+xcopy "../../openssl/Win32/Debug/lib/engines-3/*.dll" "../../../Libs/openssl/Win32/Debug/lib/engines-3/" /F /Y /E 
+
+xcopy "../../openssl/Win32/Release/bin/*" "../../../Libs/openssl/Win32/Release/bin/" /F /Y /E 
+xcopy "../../openssl/Win32/Release/include/openssl/*.h" "../../../Libs/openssl/Win32/Release/include/openssl/" /F /Y /E 
+xcopy "../../openssl/Win32/Release/lib/*.lib" "../../../Libs/openssl/Win32/Release/lib/" /F /Y /E 
+xcopy "../../openssl/Win32/Release/lib/ossl-modules/*.dll" "../../../Libs/openssl/Win32/Release/lib/ossl-modules/" /F /Y /E 
+xcopy "../../openssl/Win32/Release/lib/engines-3/*.dll" "../../../Libs/openssl/Win32/Release/lib/engines-3/" /F /Y /E 
+
+xcopy "../../openssl/x64/Debug/bin/*" "../../../Libs/openssl/x64/Debug/bin/" /F /Y /E 
+xcopy "../../openssl/x64/Debug/include/openssl/*.h" "../../../Libs/openssl/x64/Debug/include/openssl/" /F /Y /E 
+xcopy "../../openssl/x64/Debug/lib/*.lib" "../../../Libs/openssl/x64/Debug/lib/" /F /Y /E 
+xcopy "../../openssl/x64/Debug/lib/ossl-modules/*.dll" "../../../Libs/openssl/x64/Debug/lib/ossl-modules/" /F /Y /E 
+xcopy "../../openssl/x64/Debug/lib/engines-3/*.dll" "../../../Libs/openssl/x64/Debug/lib/engines-3/" /F /Y /E 
+
+xcopy "../../openssl/x64/Release/bin/*" "../../../Libs/openssl/x64/Release/bin/" /F /Y /E 
+xcopy "../../openssl/x64/Release/include/openssl/*.h" "../../../Libs/openssl/x64/Release/include/openssl/" /F /Y /E 
+xcopy "../../openssl/x64/Release/lib/*.lib" "../../../Libs/openssl/x64/Release/lib/" /F /Y /E 
+xcopy "../../openssl/x64/Release/lib/ossl-modules/*.dll" "../../../Libs/openssl/x64/Release/lib/ossl-modules/" /F /Y /E 
+xcopy "../../openssl/x64/Release/lib/engines-3/*.dll" "../../../Libs/openssl/x64/Release/lib/engines-3/" /F /Y /E 
+
+pause
