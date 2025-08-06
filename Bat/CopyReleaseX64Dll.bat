fc06c7bad428a49466963ba8a3c7f3dfa7943ac0
彭武阳 <94458936@qq.com>
2025-08-06 09:55:07 +0800

    + 辅助工程

--- Bat/CopyReleaseX64Dll.bat
+++ Bat/CopyReleaseX64Dll.bat
@@ -2,17 +2,16 @@
  
 set Platform=%1
 set Target=%2
+set LibPath=%3
 
-xcopy "%ACEX64Lib%/ACE.dll" "%Target%" /F /Y
-xcopy "%MysqlConnectorX64ReleaseLib%/*.dll" "%Target%" /F /Y
-xcopy "%FreeglutX64ReleaseBin%/freeglut.dll" "%Target%" /F /Y
-xcopy "%OpenSSLX64Lib%/../bin/*.dll" "%Target%" /F /Y
-xcopy "%OpenXLSXX64ReleaseLib%/*.dll" "%Target%" /F /Y
-xcopy "%ProtobufX64ReleaseLib%/*.dll" "%Target%" /F /Y
-xcopy "%ActiveMQX64ReleaseLib%/*.dll" "%Target%" /F /Y
-xcopy "%AprX64ReleaseLib%/*.dll" "%Target%" /F /Y
-xcopy "%MongoCxxDriverX64ReleaseLib%/*.dll" "%Target%" /F /Y
-xcopy "%BsoncxxX64ReleaseLib%/*.dll" "%Target%" /F /Y
-xcopy "%MongocxxInclude%/../Build%Platform%/mongo-c-driver/bin/mongoc*.dll" "%Target%" /F /Y
-xcopy "%MongocxxInclude%/../Build%Platform%/mongo-c-driver/bin/bson*.dll" "%Target%" /F /Y
-xcopy "%HiredisX64ReleaseLib%/*.dll" "%Target%" /F /Y
+xcopy "%LibPath%/ACE/ACE_wrappers/lib/X64/ACE.dll" "%Target%" /F /Y
+xcopy "%LibPath%/openssl/x64/Release/bin/*.dll" "%Target%" /F /Y
+xcopy "%LibPath%/mysql-connector-c++/BuildX64/Release/*.dll" "%Target%" /F /Y
+xcopy "%LibPath%/freeglut/BuildX64/bin/Release/freeglut.dll" "%Target%" /F /Y
+xcopy "%LibPath%/OpenXLSX/BuildX64/output/Release/*.dll" "%Target%" /F /Y
+xcopy "%LibPath%/protobuf/BuildX64/bin/Release/*.dll" "%Target%" /F /Y
+xcopy "%LibPath%/activemq/activemq-cpp/vs2010-build/x64/ReleaseDLL/*.dll" "%Target%" /F /Y
+xcopy "%LibPath%/apr/BuildX64/Release/*.dll" "%Target%" /F /Y
+xcopy "%LibPath%/mongo-cxx-driver/BuildX64/src/mongocxx/Release/*.dll" "%Target%" /F /Y
+xcopy "%LibPath%/mongo-cxx-driver/BuildX64/src/bsoncxx/Release/*.dll" "%Target%" /F /Y
+xcopy "%LibPath%/hiredis/BuildX64/Release/*.dll" "%Target%" /F /Y
