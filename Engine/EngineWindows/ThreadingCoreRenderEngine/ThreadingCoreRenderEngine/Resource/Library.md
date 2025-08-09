089dc7844776d9c40a42a11564b85e765a61af6b
彭武阳 <94458936@qq.com>
2025-08-09 10:42:11 +0800

    + 测试工程

--- Engine/EngineWindows/ThreadingCoreRenderEngine/ThreadingCoreRenderEngine/Resource/Library.md
+++ Engine/EngineWindows/ThreadingCoreRenderEngine/ThreadingCoreRenderEngine/Resource/Library.md
@@ -96,8 +96,8 @@
 2. 官方网站：https://github.com/protocolbuffers/protobuf。
 3. git地址：https://github.com/protocolbuffers/protobuf.git。
 4. 编译方式：CMake。Win32版本建立在BuildWin32下，x64版本建立在BuildX64下。
-5. 选中protobuf_BUILD_SHARED_LIBS。
-6. 将无法通过编译的工程的C++ 标准改成 /std:c++latest。
+5. CMakeLists.txt文件第5行增加 set(CMAKE_CXX_STANDARD 20)。
+6. 选中protobuf_BUILD_SHARED_LIBS。
 7. runtime_version.h尾部增加  
    #ifdef PROTO_BUF_PRAGMA_WARNING  
        
--- External/Bat/ProtobufBat/1. CopyProtobuf.bat
+++ External/Bat/ProtobufBat/1. CopyProtobuf.bat
@@ -7,17 +7,17 @@ xcopy "../../protobuf/src/*.inc" "../../../Libs/protobuf/src/" /F /Y /E
 xcopy "../../protobuf/BuildX64/_deps/absl-src/*.h" "../../../Libs/protobuf/src/" /F /Y /E  
 xcopy "../../protobuf/BuildX64/_deps/absl-src/*.inc" "../../../Libs/protobuf/src/" /F /Y /E  
 
-xcopy "../../protobuf/BuildWin32/bin/Debug/*.lib" "../../../Libs/protobuf/BuildWin32/bin/Debug/" /F /Y /E  
-xcopy "../../protobuf/BuildWin32/bin/Debug/*.dll" "../../../Libs/protobuf/BuildWin32/bin/Debug/" /F /Y /E  
-xcopy "../../protobuf/BuildWin32/bin/Debug/*.exe" "../../../Libs/protobuf/BuildWin32/bin/Debug/" /F /Y /E  
+xcopy "../../protobuf/BuildWin32/bin/Release/*.lib" "../../../Libs/protobuf/BuildWin32/bin/Debug/" /F /Y /E  
+xcopy "../../protobuf/BuildWin32/bin/Release/*.dll" "../../../Libs/protobuf/BuildWin32/bin/Debug/" /F /Y /E  
+xcopy "../../protobuf/BuildWin32/bin/Release/*.exe" "../../../Libs/protobuf/BuildWin32/bin/Debug/" /F /Y /E  
 
 xcopy "../../protobuf/BuildWin32/bin/Release/*.lib" "../../../Libs/protobuf/BuildWin32/bin/Release/" /F /Y /E  
 xcopy "../../protobuf/BuildWin32/bin/Release/*.dll" "../../../Libs/protobuf/BuildWin32/bin/Release/" /F /Y /E  
 xcopy "../../protobuf/BuildWin32/bin/Release/*.exe" "../../../Libs/protobuf/BuildWin32/bin/Release/" /F /Y /E  
 
-xcopy "../../protobuf/BuildX64/bin/Debug/*.lib" "../../../Libs/protobuf/BuildX64/bin/Debug/" /F /Y /E  
-xcopy "../../protobuf/BuildX64/bin/Debug/*.dll" "../../../Libs/protobuf/BuildX64/bin/Debug/" /F /Y /E 
-xcopy "../../protobuf/BuildX64/bin/Debug/*.exe" "../../../Libs/protobuf/BuildX64/bin/Debug/" /F /Y /E   
+xcopy "../../protobuf/BuildX64/bin/Release/*.lib" "../../../Libs/protobuf/BuildX64/bin/Debug/" /F /Y /E  
+xcopy "../../protobuf/BuildX64/bin/Release/*.dll" "../../../Libs/protobuf/BuildX64/bin/Debug/" /F /Y /E 
+xcopy "../../protobuf/BuildX64/bin/Release/*.exe" "../../../Libs/protobuf/BuildX64/bin/Debug/" /F /Y /E   
 
 xcopy "../../protobuf/BuildX64/bin/Release/*.lib" "../../../Libs/protobuf/BuildX64/bin/Release/" /F /Y /E  
 xcopy "../../protobuf/BuildX64/bin/Release/*.dll" "../../../Libs/protobuf/BuildX64/bin/Release/" /F /Y /E  
