1d20afbdc2950268e7498f0f41ccc83b9a7c9220
彭武阳 <94458936@qq.com>
2025-08-12 10:05:13 +0800

    + 测试工程

--- External/Bat/MinizBat/1. CopyMiniz.bat
+++ External/Bat/MinizBat/1. CopyMiniz.bat
@@ -5,10 +5,10 @@ rmdir /S /Q "../../../Libs/miniz"
 xcopy "../../miniz/*.h" "../../../Libs/miniz/" /F /Y /E  
 xcopy "../../miniz/BuildX64/miniz_export.h" "../../../Libs/miniz/" /F /Y /E  
 
-xcopy "../../miniz/BuildWin32/Debug/*.lib" "../../../Libs/miniz/BuildWin32/Debug/" /F /Y /E 
-xcopy "../../miniz/BuildWin32/Release/*.lib" "../../../Libs/miniz/BuildWin32/Release/" /F /Y /E   
+xcopy "../../miniz/BuildWin32/Debug/*.*" "../../../Libs/miniz/BuildWin32/Debug/" /F /Y /E 
+xcopy "../../miniz/BuildWin32/Release/*.*" "../../../Libs/miniz/BuildWin32/Release/" /F /Y /E   
 
-xcopy "../../miniz/BuildX64/Debug/*.lib" "../../../Libs/miniz/BuildX64/Debug/" /F /Y /E 
-xcopy "../../miniz/BuildX64/Release/*.lib" "../../../Libs/miniz/BuildX64/Release/" /F /Y /E
+xcopy "../../miniz/BuildX64/Debug/*.*" "../../../Libs/miniz/BuildX64/Debug/" /F /Y /E 
+xcopy "../../miniz/BuildX64/Release/*.*" "../../../Libs/miniz/BuildX64/Release/" /F /Y /E
 
 pause
